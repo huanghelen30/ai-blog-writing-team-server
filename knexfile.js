d94d07c74763bea5bbc7ff1fb5549446b723626f
Helen <huanghelen30@gmail.com>
2025-03-13 11:45:27 -0400

    create knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -4,11 +4,19 @@ import "dotenv/config";
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 export default {
-  client: 'mysql2',
-  connection: {
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-  }
+  development: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    migrations: {
+      directory: './migrations',
+    },
+    seeds: {
+      directory: './seeds',
+    },
+  },
 };
